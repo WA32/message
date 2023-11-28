535958e163cbf8428916abab1271de5b2307105e
Akmal Wiraprana <akmalwiraprana26@gmail.com>
2023-11-28 22:23:25 +0700

    add

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ yesBtn.addEventListener("click", () => {
   question.innerHTML = "Yay, see you on the 18th!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+  
 });
 
 noBtn.addEventListener("mouseover", () => {
